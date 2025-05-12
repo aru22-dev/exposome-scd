02610037175aef8a779fc8b4d1557af8125801da
Arundhati Raj <arundhati.official2024@gmail.com>
2025-05-12 21:32:56 +0530

    Updated server default to fallback on

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
